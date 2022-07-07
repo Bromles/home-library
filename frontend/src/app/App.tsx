95d8b6da285e72c32078d6035919f3ea71801f47
Bromles <alexandr.v.u@gmail.com>
2022-07-07 13:03:32 +0300

    add theme changing to frontend

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,50 +1,30 @@
-import {useState} from 'react';
 import logo from '../logo.svg';
 import styles from './App.module.css';
 import store from "./AppStore";
 import {observer} from "mobx-react-lite";
 import {Button} from "primereact/button";
-
-if (store.useDarkTheme) {
-    await import('primereact/resources/themes/lara-dark-teal/theme.css')
-} else {
-    await import('primereact/resources/themes/lara-light-teal/theme.css')
-}
-
 import "primeicons/primeicons.css";
 import {PrimeIcons} from "primereact/api";
 
 const App = observer(() => {
-    const [count, setCount] = useState(0)
+    const changeTheme = () => {
+        const link = document.getElementById('app-theme')
+        store.useDarkTheme = !store.useDarkTheme;
+        link?.setAttribute('href', store.useDarkTheme ? '/src/dark.css' : '/src/light.css')
+    }
 
     return (
         <div className={styles.app}>
             <header className={styles.appHeader}>
                 <img src={logo} className={styles.appLogo} alt="logo"/>
                 <p>Hello Vite + React!</p>
-                <p>
-                    <button type="button" onClick={() => setCount((c) => c + 1)}>
-                        count is: {count}
-                    </button>
-                </p>
                 <div>
                     <Button icon={store.useDarkTheme ? PrimeIcons.SUN : PrimeIcons.MOON}
-                            className="themeButton p-button-outlined"
-                            onClick={() => store.useDarkTheme = !store.useDarkTheme}>
+                            className={styles.appThemeButton}
+                            onClick={() => changeTheme()}>
                     </Button>
                 </div>
                 <p>
-                    Edit <code>App.tsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className={styles.appLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {' | '}
                     <a
                         className={styles.appLink}
                         href="https://vitejs.dev/guide/features.html"
