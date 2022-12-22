75937ff2124de9cb66b8219dc7211e49462c08dd
Bromles <alexandr.v.u@gmail.com>
2022-12-23 02:03:20 +0300

    add auth guard

--- frontend/web/src/app/app-routing.module.ts
+++ frontend/web/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {AuthGuard} from "./auth.guard";
 
 const routes: Routes = [
     {
@@ -25,7 +26,8 @@ const routes: Routes = [
     },
     {
         path: 'settings',
-        loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule)
+        loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule),
+        canActivate: [AuthGuard]
     },
     {
         path: '**',
