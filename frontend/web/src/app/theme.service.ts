7df8541630ce3dd3c86b9d5d61c34688780a7e9a
Bromles <alexandr.v.u@gmail.com>
2022-12-22 20:08:03 +0300

    done theme service

--- frontend/web/src/app/theme.service.ts
+++ frontend/web/src/app/theme.service.ts
@@ -1,25 +1,42 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {StyleManagerService} from "./style-manager.service";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ThemeService {
-  isDarkTheme$: boolean;
+    private darkThemeSubject: BehaviorSubject<boolean>;
+    isDarkTheme$: Observable<boolean>;
 
-  constructor(private styleManager: StyleManagerService) {
-    this.setInitialTheme();
-  }
+    constructor(private styleManager: StyleManagerService) {
+        this.darkThemeSubject = new BehaviorSubject<boolean>(this.isInitialThemeDark());
+        this.isDarkTheme$ = this.darkThemeSubject.asObservable();
+        this.setInitialTheme()
+    }
 
-  setInitialTheme() {
-    this.isDarkTheme$ = true;
-  }
+    private isInitialThemeDark() {
+        const localStorageThemeDark = localStorage.getItem("isDarkTheme");
 
-  toggleTheme() {
-    this.styleManager.setStyle(
-        "theme",
-        "assets/theme-dark.css"
-    )
-  }
+        if (localStorageThemeDark) {
+            return localStorageThemeDark === "true";
+        }
+
+        return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+
+    private setInitialTheme() {
+        this.setTheme(this.darkThemeSubject.value);
+    }
+
+    private setTheme(dark: boolean) {
+        this.darkThemeSubject.next(dark);
+        localStorage.setItem("isDarkTheme", dark.toString());
+
+        this.styleManager.setStyle("theme", dark ? "assets/dark-theme.css" : "assets/light-theme.css");
+    }
+
+    toggleTheme() {
+        this.setTheme(!this.darkThemeSubject.value);
+    }
 }
