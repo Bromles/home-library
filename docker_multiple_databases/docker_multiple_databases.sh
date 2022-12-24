eadae94d9ce3c9d2237f2d70975b8dc8f9d2596d
Bromles <alexandr.v.u@gmail.com>
2022-12-24 20:17:59 +0300

    change postgres version to 13.9

--- docker_multiple_databases/docker_multiple_databases.sh
+++ docker_multiple_databases/docker_multiple_databases.sh
@@ -8,6 +8,7 @@ function create_user_and_database() {
 	echo "  Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $database;
+	    ALTER USER $database WITH PASSWORD '$POSTGRES_PASSWORD';
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
