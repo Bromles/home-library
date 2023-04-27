68e3b8bc90b2569b695e85bade408b381fafbbb0
ruarsv5 <Vadim.Ershov@raiffeisen.ru>
2023-04-27 23:05:10 +0300

    e2e init

--- frontend/web/src/environments/environment.prod.ts
+++ frontend/web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     authIssuer: "http://localhost:8099/realms/home_library",
-    backendUrl: 'http://localhost:8082/api/v1'
+    backendUrl: 'http://localhost:8082/'
 }
