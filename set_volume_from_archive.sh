51d8d144297752a2c0c5f4c821d13a4848ae0247
ruarsv5 <Vadim.Ershov@raiffeisen.ru>
2023-04-27 23:58:02 +0300

    e2e init

--- set_volume_from_archive.sh
+++ set_volume_from_archive.sh
@@ -1,7 +1,7 @@
 BACKUP_PATH=./volume_backup &&
 BACKUP_DOCKER_PATH=volume_backup &&
 VOLUME_NAME=home-library_db-data &&
-cat $BACKUP_DOCKER_PATH/db-data_archive.tar.bz2.parta* > db-data_archive.tar.bz2 &&
-$pwd &&
+cat $BACKUP_DOCKER_PATH/db-data_archive.tar.bz2.parta* > $BACKUP_DOCKER_PATH/db-data_archive.tar.bz2 &&
+pwd &&
 ls -la &&
 docker run --rm -v $VOLUME_NAME:/volume -v $BACKUP_DOCKER_PATH:/backup alpine sh -c "cd backup; ls -la; cd ..; rm -rf /volume/* /volume/..?* /volume/.[!.]* ; tar -C /volume/ -xjf /backup/db-data_archive.tar.bz2"
